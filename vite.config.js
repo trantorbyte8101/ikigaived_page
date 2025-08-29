3b60fe6a49eb13861c1eb691510e61aaae4e4f07
Omar <trantorbyte8101@gmail.com>
2025-08-29 15:46:16 -0600

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ikigaived_page/', // ← Reemplaza con el nombre de tu repo
+  base: '/ikigaived_page/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
